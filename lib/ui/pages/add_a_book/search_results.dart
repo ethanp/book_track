32884c98d1406964e06a5fd8be1b968e65697960
Ethan Petuchowski <etahnp@gmail.com>
2024-11-16 18:40:17 -0600

    Reorg

--- lib/ui/pages/add_a_book/search_results.dart
+++ lib/ui/pages/add_a_book/search_results.dart
@@ -32,8 +32,6 @@ class SearchResults extends ConsumerWidget {
   }
 
   Widget item(Book book, WidgetRef ref) {
-    final String subtitle =
-        [book.bookType, book.bookLengthPgs].where((v) => v != null).join(' ');
     return Container(
       margin: const EdgeInsets.all(1),
       child: InkWell(
@@ -41,36 +39,53 @@ class SearchResults extends ConsumerWidget {
           elevation: .3,
           child: Row(
             children: [
-              SizedBox(
-                width: 50,
-                child: book.coverArtS == null
-                    ? null
-                    : Image.memory(book.coverArtS!),
-              ),
-              Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text(book.title,
-                      style:
-                          TextStyle(fontSize: 16, fontWeight: FontWeight.w700)),
-                  Text(
-                    book.author ?? 'No author listed',
-                    style: TextStyle(
-                      fontStyle: FontStyle.italic,
-                      fontWeight: FontWeight.w500,
-                      color: Colors.grey[800],
-                    ),
-                  ),
-                  if (subtitle.isNotEmpty)
-                    Text(subtitle,
-                        style: TextStyle(fontWeight: FontWeight.w400)),
-                ],
-              ),
+              coverArt(book),
+              bookInfo(book),
             ],
           ),
         ),
         onTap: () => ref.context.push(SearchResultDetailPage(book)),
       ),
+    );
+  }
+
+  Widget coverArt(Book book) {
+    return SizedBox(
+      width: 50,
+      child: book.coverArtS == null ? null : Image.memory(book.coverArtS!),
+    );
+  }
+
+  Widget bookInfo(Book book) {
+    final String subtitle =
+        [book.bookType, book.bookLengthPgs].where((v) => v != null).join(' ');
+    final title = Text(
+      book.title,
+      style: TextStyle(
+        fontSize: 16,
+        fontWeight: FontWeight.w700,
+      ),
+    );
+    final author = Text(
+      book.author ?? 'No author listed',
+      style: TextStyle(
+        fontStyle: FontStyle.italic,
+        fontWeight: FontWeight.w500,
+        color: Colors.grey[800],
+      ),
+    );
+    final otherInfo = [
+      if (subtitle.isNotEmpty)
+        Text(
+          subtitle,
+          style: TextStyle(
+            fontWeight: FontWeight.w400,
+          ),
+        ),
+    ];
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [title, author, ...otherInfo],
     );
   }
 }
